66c33cf7ab0d055ee3681ac3056ed7f228e3a7d1
Richard J. Burman <50410029+richardjburman@users.noreply.github.com>
2022-10-07 17:38:56 +0200

    RJB_IOM_Updates
    Edited Matlab edf_IOM preprocessng
    Edited kriging_artifacts_IOM
    
    Created virtual environment for Python code

--- Matlab_preprocessing/edf_IOM.m
+++ Matlab_preprocessing/edf_IOM.m
@@ -1,29 +1,28 @@
 %% edf file
-filepath = 'F:\raw data anonimized\anonymous_2022-09-23_14-37-36\c0685166-ffa7-46a1-aa73-d8b302eb910c\387d08a6-6e0c-41df-a1ad-2b24ccade91e\20220923_103625_827.SEPt_62d44fa8-c7f7-4614-8b07-9008a50e6ff8.edf';
+filepath               = 'C:\Users\rburman.MSDITUN-TMV0GCR\Dropbox\Postdoc\projects\coeus\data\anonymous_2022-09-23_14-37-36\ccep_protocol\20220923_103625_827.SEPt_62d44fa8-c7f7-4614-8b07-9008a50e6ff8.edf';
 H                      = Epitome_edfExtractHeader(filepath);
 [hdr_edf, EEG_SEP]     = edfread_data(filepath);
 %% plots
 Fs             = round(hdr_edf.frequency(1)); % recording Fs
-clf(figure(1))
 
 c = 1;
 t      = 2000000;
 x_s = 10;
 %x_ax        = -x_s:1/Fs:x_s;
 x_ax        = 1:1/Fs:20*60;
-plot(EEG_SEP(c,:));
-hold on 
-plot(EEG_pp(c,:));
-hold on 
-plot(EEG_filter(c,:));
-title(hdr_edf.label(c))
-%%
+
+%% stimulus artefact removal
+Fs             = round(hdr_edf.frequency(1)); % recording Fs
 
 EEG_pp = EEG_SEP;
+
+[h,w] = size(EEG_SEP);
+
 for i=1:1200
     trig = i*Fs;
-    for c=1:height(EEG_SEP)
-        EEG_pp(c,:) = kriging_artifacts_LT(EEG_pp(c,:), trig,trig, 0, Fs,0);
+    
+    for c=1:h
+        EEG_pp(c,:) = kriging_artifacts_IOM(EEG_pp(c,:), trig,trig, 0, Fs,0);
     end
 end
 
@@ -42,3 +41,14 @@ end
 % EEG_scaled          = bsxfun(@rdivide,sclA*EEG_filter,sclC);
 
 % ppEEG               = resample(EEG_scaled',Fs2,Fs)';
+
+
+%%
+clf(figure(1))
+plot(EEG_SEP(c,:),'b');
+hold on 
+plot(EEG_pp(c,:),'r');
+hold on 
+plot(EEG_filter(c,:),'g');
+
+title(hdr_edf.label(c))
---
+++ Matlab_preprocessing/nx_preproc/kriging_artifacts_IOM.m
@@ -0,0 +1,28 @@
+%adapated by RJB for IOM CCEPsssss - 7.10.22
+
+function [EEG_c] = kriging_artifacts_LT(EEG_c, trig1, trig2, IPI, Fs, scalp)
+     [pk_op, pk_lp, pk_cl,pk_stim, pk_stim2] = find_artifacts_pk(EEG_c, trig1, trig2, IPI, Fs);
+    
+    d_pk            = round([0.005 0.005]*Fs);
+
+    if scalp
+        d_pk_stim       = round([0.005 0.015]*Fs);%[0.005 0.015]*Fs;, 2-5
+    else % duration to remove
+        %d_pk_stim       = round([0.005 0.015]*Fs);%[0.005 0.015]*Fs;, 2-8 , EL003:7-15
+        d_pk_stim       = round([0.002 0.030]*Fs);%[0.005 0.015]*Fs;, 2-8 , EL003:7-15
+    end
+
+    le = round(0.01*Fs);%(pk_stim-d_pk_stim(1)-2) - (pk_op+d_pk(2)+1);
+
+    %stimulations peaks
+    
+    EEG_c(pk_stim-d_pk_stim(1):pk_stim+d_pk_stim(2)) = kriging_func(EEG_c, pk_stim, d_pk_stim,3);
+    
+%     EEG_c(pk_stim-d_pk_stim(1):pk_stim+d_pk_stim(2)) = pre_mean;
+    if ~isempty(pk_stim2)
+        
+        EEG_c(pk_stim2-d_pk_stim(1):pk_stim2+d_pk_stim(2)) = kriging_func(EEG_c, pk_stim2, d_pk_stim,3);%0;
+    end
+end
+
+    
